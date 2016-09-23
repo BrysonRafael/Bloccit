a5cb5af226b27552f438e1711017489ca8f44da6
Bryson <bryson.rafael@gmail.com>
2016-09-23 14:24:28 -0500

    Assignment 17

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,20 @@ posts = Post.all
   )
 end
 
+1.times do
+  Post.find_or_create_by!(
+    title: "Asparagus",
+    body: "Makes your urine smell funny"
+  )
+end
+
+1.times do
+  Comment.find_or_create_by!(
+    post: posts.sample,
+    body: "You should try eating beets sometime!"
+  )
+end
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
