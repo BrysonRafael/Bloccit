61673c451c12764bc4b41baeb3dea2f1577887c2
Bryson <bryson.rafael@gmail.com>
2016-09-26 11:26:43 -0500

    Removed 1.times do + fixed syntax

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,19 +16,22 @@ posts = Post.all
   )
 end
 
-1.times do
-  Post.find_or_create_by!(
-    title: "Asparagus",
-    body: "Makes your urine smell funny"
-  )
-end
+puts "#{Post.count}"
 
-1.times do
-  Comment.find_or_create_by!(
-    post: posts.sample,
-    body: "You should try eating beets sometime!"
+Post.find_or_create_by(
+    title: 'Asparagus',
+    body: 'Makes your urine smell funny'
   )
-end
+
+puts "#{Post.count}"
+
+puts "#{Comment.count}"
+
+Comment.find_or_create_by(
+    body: 'You should try eating beets sometime!'
+  )
+
+puts "#{Comment.count}"
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
