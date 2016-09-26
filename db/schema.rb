639ec0ecdff3814b40e571553995f24ae3034911
Bryson <bryson.rafael@gmail.com>
2016-09-26 12:16:36 -0500

    Assignment Revision: Changed Post_id to Question_id

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,12 @@ ActiveRecord::Schema.define(version: 20160923153849) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
-    t.integer  "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "question_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
-  add_index "answers", ["post_id"], name: "index_answers_on_post_id"
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id"
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -34,9 +34,12 @@ ActiveRecord::Schema.define(version: 20160923153849) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
+    t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "posts", ["post_id"], name: "index_posts_on_post_id"
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
