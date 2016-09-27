a0888b893bf4c7ca6dd2a11aa2f11107abe63dd7
Bryson <bryson.rafael@gmail.com>
2016-09-27 12:42:36 -0500

    Assignment 18

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ActiveRecord::Base
   has_many :comments
+
+  posts.each do |post|
+    post = post[i]
+      if post % 5 == 0
+        post.title = 'SPAM'
+      end
+    end
+  end
+
 end
