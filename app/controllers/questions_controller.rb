5c16e1533604bf905017afe3bedf1a68f6968ed8
Bryson <bryson.rafael@gmail.com>
2016-10-02 11:16:03 -0500

    Assignment 20: Need Revision

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,25 +3,23 @@ class QuestionsController < ApplicationController
     @questions = Question.all
   end
 
+  def show
+    @question = Question.find(params[:id])
+  end
+
   def new
     @question = Question.new
   end
 
   def create
-    @question = Question.new
-    @question.title = params[:question][:title]
-    @question.body = params[:question][:body]
+    @question = Question.new(params.require(:question).permit(:title, :body, :resolved))
     if @question.save
       flash[:notice] = "Question was saved."
       redirect_to @question
     else
-      flash.now[:alert] = "There was an error saving the question. Please try again."
+      flash[:error] = "There was an error saving the question. Please try again."
       render :new
     end
-  end
-
-  def show
-    @question = Question.find(params[:id])
   end
 
   def edit
@@ -30,14 +28,11 @@ class QuestionsController < ApplicationController
 
   def update
     @question = Question.find(params[:id])
-    @question.title = params[:question][:title]
-    @question.body = params[:question][:body]
-
-    if @question.save
+    if @question.update_attributes(params.require(:question).permit(:title, :body, :resolved))
       flash[:notice] = "Question was updated."
       redirect_to @question
     else
-      flash.now[:alert] = "There was an error saving the question. Please try again."
+      flash[:error] = "There was an error saving the question. Please try again."
       render :edit
     end
   end
@@ -49,8 +44,8 @@ class QuestionsController < ApplicationController
       flash[:notice] = "\"#{@question.title}\" was deleted successfully."
       redirect_to questions_path
     else
-      flash.now[:alert] = "There was an error deleting the question."
-      render :show
+      flash[:error] = "There was an error deleting the question."
+      render "show"
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   resources :posts, :questions
 
   get 'about' => 'welcome#about'
