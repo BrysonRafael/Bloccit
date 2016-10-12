8a78966fb9bf8ed9e0e2fe6485f3c7ff74b10c86
Bryson <bryson.rafael@gmail.com>
2016-10-12 12:38:42 -0500

    Randome Number changed to static 9

--- spec/controllers/sponsored_posts_controller_spec.rb
+++ spec/controllers/sponsored_posts_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe SponsoredPostsController, type: :controller do
   let(:my_topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
-  let(:my_sponsored_post) { my_topic.sponsoredposts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, price: rand(9)) }
+  let(:my_sponsored_post) { my_topic.sponsoredposts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, price: 9 }
 
   describe "GET #show" do
     it "returns http success" do
