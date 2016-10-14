f16c9c6e731afb4c320b8629321200484510ee6b
Bryson <bryson.rafael@gmail.com>
2016-10-14 13:54:11 -0500

    Assignment 25: Need Help

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
+  resources :users, only: [:confirm, :confirm]
+
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
