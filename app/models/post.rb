9091d40077fd4ab4b9d8528efbcba04f07efaed2
Bryson <bryson.rafael@gmail.com>
2016-10-24 14:23:12 -0500

    Assignment 31: Need Help

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  after_create :favorite_user_post(user, post)
 
   default_scope { order('rank DESC') }
   validates :title, length: { minimum: 5}, presence: true
@@ -27,5 +28,11 @@ class Post < ActiveRecord::Base
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
+  end
+
+  private
+
+  def favorite_user_post(user, post)
+    favorites.where(user: current_user, post_id: post.id).first
   end
 end
